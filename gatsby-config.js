3a97a72e93757550644061feba1fe0f7c5c4ed87
Ali Ahsan <aliahsan@Alis-MacBook-Pro.local>
2019-12-13 21:32:13 -0600

    Favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,18 +84,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+        icon: `src/images/owl.svg`
       }
     },
     "gatsby-plugin-offline",
