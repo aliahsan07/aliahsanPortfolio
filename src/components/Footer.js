a6e23faca0bd4123e6d7ea325a297be93d2c4cdf
AbreezaSaleem <abreeza.saleem@hotmail.com>
2020-08-11 01:15:17 +0500

    footer issue fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer-container">
+      <div>
       <footer className="footer">
         <div>
           <a
